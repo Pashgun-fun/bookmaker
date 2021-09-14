8c8b20671728cc3f40b2312b466f1dd2aac4430e
Pavel <71293297+Tefkal1on@users.noreply.github.com>
2021-09-14 23:48:47 +0300

    Finaly end a bookmakers cards
    Rewrite file function.js

--- js/function.js
+++ js/function.js
@@ -1,21 +1,38 @@
-let arr = document.querySelectorAll('.rating__item');
 
-function removeClass(array){
-    for (let i = 0; i < array.length; i++){
-        for (let j = 0; j < arguments.length; j++){
-            arr[i].classList.remove(arguments[j])
-        }
-    }
+let num = document.querySelectorAll('.bookmaker__number');
+let rev = document.querySelectorAll('.bookmaker__review');
+let bookmakers = document.querySelectorAll('.bookmakers__item');
+
+function rand(min, max){
+    return Math.floor(Math.random()*(max - min))
 }
 
-arr.forEach((item, index) => {
+rev.forEach(item => {
+    item.querySelector('span').innerHTML = `(${rand(1,99)})`
+})
+
+
+num.forEach((item, index) => {
+    if(item < 10){
+        item.textContent = "0" + index + 1;
+    }else{
+        item.textContent = index + 1;
+    }
+})
+
+bookmakers.forEach((item) => {
     item.addEventListener('mouseover', e => {
-        for (let i = 0; i < arr.length; i++){
-            arr[i].classList.remove('active-star');
-        }
-        let indexEl = index;
-        for (let j = 0; j <= indexEl; j++){
-            arr[j].classList.add('active-star');
-        }
+        let stars = item.querySelectorAll('.rating__item');
+        stars.forEach((element, index) => {
+            element.addEventListener('mouseover', e => {
+                for(let i = 0; i < stars.length; i++){
+                    stars[i].classList.remove('active-star');
+                }
+                let indexEl = index;
+                for (let j = 0; j <= indexEl; j++){
+                    stars[j].classList.add('active-star');
+                }
+            })
+        })
     })
 })
