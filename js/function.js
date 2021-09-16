b5645799854bffe3648c0cd5e2e2c14b0bc3cc46
Pavel <71293297+Tefkal1on@users.noreply.github.com>
2021-09-16 09:30:49 +0300

    Change files

--- js/function.js
+++ js/function.js
@@ -1,16 +1,27 @@
-
 let num = document.querySelectorAll('.bookmaker__number');
 let rev = document.querySelectorAll('.bookmaker__review');
+let bookmakerList = document.querySelector('.bookmakers__list');
 let bookmakers = document.querySelectorAll('.bookmakers__item');
+let marginBottom = parseInt(getComputedStyle(bookmakers[0], true).marginBottom);
+let max = (bookmakers[0].offsetHeight + marginBottom) * 6;
+let more = document.querySelector('.bookmakers__more');
 
 function rand(min, max){
     return Math.floor(Math.random()*(max - min))
 }
 
+function hide(max) {
+    if(bookmakerList.offsetHeight > max){
+        bookmakerList.style.height = max + 'px';
+        bookmakerList.style.overflow = 'hidden';
+    }
+}
+
+hide(max)
+
 rev.forEach(item => {
     item.querySelector('span').innerHTML = `(${rand(1,99)})`
 })
-
 
 num.forEach((item, index) => {
     if(item < 10){
@@ -35,4 +46,13 @@ bookmakers.forEach((item) => {
             })
         })
     })
+})
+
+more.addEventListener('click', e => {
+    e.preventDefault();
+    if (parseInt(getComputedStyle(bookmakerList, true).height) >= (bookmakers[0].offsetHeight + marginBottom) * bookmakers.length){
+        bookmakerList.style.height = bookmakerList.offsetHeigth + 'px';
+    }else {
+        bookmakerList.style.height = parseInt(getComputedStyle(bookmakerList, true).height) + (max / 2) + 'px';
+    }
 })
